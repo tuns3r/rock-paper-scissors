4868734995e833023e78cabb92e52a5f0664d20f
Tyler <ttunser@hotmail.ca>
2022-03-02 06:16:54 -0700

    Having a issue where if user doesn't input rock (since it is the first if statement) it won't work. Also created the game function with the for statement and can't get it to run more then once. Made some progress this morning but found more issues to work through.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,75 @@
 function computerPlay(number) {
     if (number === 0) {
-        return "Computer Selected: Rock"; 
+        return "Rock"; 
     } else if (number === 1) {
-        return "Computer Selected: Paper"
+        return "Paper"
     } else if (number === 2) {
-        return "Computer Selected: Scissors"
+        return "Scissors"
     } else {
         return "Something else went wrong"
     }
 }
 
-function playerSelection(item) {
+function playerPlay(item) {
     if (item == 'rock') {
-        return "User Selected : Rock"
+        return "Rock"
     } else if (item == 'paper') {
-        return "User Selected : Paper"
+        return "Paper"
     } else if (item == 'scissors') {
-        return "User Selected : Scissors"
+        return "Scissors"
     } else {
         return "Invalid Item"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie."
+    }
+
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return "You lose! Paper beats Rock.";  
+            computerScore++; 
+        } else if (computerSelection === 'Scissors') {
+            return "You win! Rock beats Scissors."
+            playerScore++; 
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerPlay === 'Rock') {
+            return "You win! Paper beats rock."
+            playerScore++; 
+        } else if (computerPlay === 'Scissors') {
+            return "You lose! Scissors beats Paper."
+            computerScore++; 
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerPlay === 'Rock') {
+            return "You lose! Rock beats Scissors."
+            computerScore++; 
+        }   else if (computerPlay === 'Paper') {
+            return "You win! Scissors beats Paper."
+            playerScore++; 
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+        return "Computer score: " + computerScore; 
+        return "Player Score: " + playerScore; 
+        i++; 
     }
 }
 
 let prompt = window.prompt("Enter Rock, Paper or Scissors: ")
 let randomNumber = Math.floor(Math.random() * 3); 
 
-console.log(playerSelection(prompt.toLowerCase()));
-console.log(computerPlay(randomNumber)); 
+const computerSelection = computerPlay(randomNumber); 
+const playerSelection = playerPlay(prompt.toLowerCase()); 
+
+let computerScore = 0; 
+let playerScore = 0; 
+
+game(); 
